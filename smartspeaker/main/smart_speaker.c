c3fdaf2cc83e93834d011b9988e41f518f4dd27c
Stef <s.kraaijeveld3@student.avans.nl>
2024-02-28 14:15:09 +0100

    Add lcd example code
    TODO: refactor

--- smartspeaker/main/smart_speaker.c
+++ smartspeaker/main/smart_speaker.c
@@ -22,6 +22,87 @@
 
 #define ARRAY_SIZE(a) ((sizeof a) / (sizeof a[0]))
 
+#include "driver/gpio.h"
+#include "driver/i2c.h"
+#include "esp_system.h"
+#include "i2c-lcd1602.h"
+#include "rom/uart.h"
+#include "sdkconfig.h"
+#include "smbus.h"
+#include <string.h>
+
+// LCD2004
+#define LCD_NUM_ROWS            4
+#define LCD_NUM_COLUMNS         40
+#define LCD_NUM_VISIBLE_COLUMNS 20
+
+#define I2C_MASTER_NUM        I2C_NUM_0
+#define I2C_MASTER_TX_BUF_LEN 0 // disabled
+#define I2C_MASTER_RX_BUF_LEN 0 // disabled
+#define I2C_MASTER_FREQ_HZ    100000
+#define I2C_MASTER_SDA_IO     18
+#define I2C_MASTER_SCL_IO     23
+
+static const char *lcdTag = "LCD";
+
+static void i2c_master_init(void) {
+	int i2c_master_port = I2C_MASTER_NUM;
+	i2c_config_t conf;
+	conf.mode          = I2C_MODE_MASTER;
+	conf.sda_io_num    = I2C_MASTER_SDA_IO;
+	conf.sda_pullup_en = GPIO_PULLUP_DISABLE; // GY-2561 provides 10kΩ pullups
+	conf.scl_io_num    = I2C_MASTER_SCL_IO;
+	conf.scl_pullup_en = GPIO_PULLUP_DISABLE; // GY-2561 provides 10kΩ pullups
+	conf.master.clk_speed = I2C_MASTER_FREQ_HZ;
+	i2c_param_config(i2c_master_port, &conf);
+	i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_LEN,
+	                   I2C_MASTER_TX_BUF_LEN, 0);
+}
+
+void lcd1602_task(void *pvParameter) {
+	// Set up I2C
+	i2c_master_init();
+	i2c_port_t i2c_num = I2C_MASTER_NUM;
+	uint8_t address    = CONFIG_LCD1602_I2C_ADDRESS;
+
+	// Set up the SMBus
+	smbus_info_t *smbus_info = smbus_malloc();
+	ESP_ERROR_CHECK(smbus_init(smbus_info, i2c_num, address));
+	ESP_ERROR_CHECK(smbus_set_timeout(smbus_info, 1000 / portTICK_RATE_MS));
+
+	// Set up the LCD1602 device with backlight off
+	i2c_lcd1602_info_t *lcd_info = i2c_lcd1602_malloc();
+	ESP_ERROR_CHECK(i2c_lcd1602_init(lcd_info, smbus_info, true, LCD_NUM_ROWS,
+	                                 LCD_NUM_COLUMNS, LCD_NUM_VISIBLE_COLUMNS));
+
+	ESP_ERROR_CHECK(i2c_lcd1602_reset(lcd_info));
+
+	// turn off backlight
+	ESP_LOGI(lcdTag, "backlight off");
+	i2c_lcd1602_set_backlight(lcd_info, false);
+
+	// turn on backlight
+	ESP_LOGI(lcdTag, "backlight on");
+	i2c_lcd1602_set_backlight(lcd_info, true);
+
+	// ESP_LOGI(TAG, "cursor on");
+	// i2c_lcd1602_set_cursor(lcd_info, true);
+
+	while (1) {
+		i2c_lcd1602_move_cursor(lcd_info, 2, 2);
+		char my_string[] = "Hello, World!";
+
+		for (int i = 0; i < strlen(my_string); i++) {
+			i2c_lcd1602_write_char(lcd_info, my_string[i]);
+			vTaskDelay(250 / portTICK_RATE_MS);
+		}
+
+		i2c_lcd1602_clear(lcd_info);
+	}
+
+	vTaskDelete(NULL);
+}
+
 static const char *TAG = "MAIN";
 
 static audio_board_handle_t board_handle;
@@ -117,6 +198,7 @@ static void pipeline_destroy(audio_deinit_fn deinit_fn,
 
 void app_main(void) {
 	app_init();
+	xTaskCreate(&lcd1602_task, "lcd1602_task", 4096, NULL, 5, NULL);
 	pipeline_init(bt_pipeline_init, i2s_stream_writer);
 
 	player_volume = 50;
