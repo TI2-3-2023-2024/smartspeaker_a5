364d6104d8a23e60c0b2eaf71025a5736025f368
GertieMeneer <gertjan.vat@icloud.com>
2024-02-28 13:58:15 +0100

    added code for sending commands to ledcontroller

--- smartspeaker/main/smart_speaker.c
+++ smartspeaker/main/smart_speaker.c
@@ -9,6 +9,7 @@
 
 #include "audio_element.h"
 #include "audio_pipeline.h"
+#include "driver/i2c.h"
 #include "i2s_stream.h"
 
 #include "esp_peripherals.h"
@@ -19,6 +20,15 @@
 #include "esp_check.h"
 #include "esp_err.h"
 #include "esp_log.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include <stdio.h>
+
+#define ARRAY_SIZE(a) ((sizeof a) / (sizeof a[0]))
+
+enum led_effects { LED_OFF, LED_ON };
 
 static const char *TAG = "MAIN";
 
@@ -104,6 +114,44 @@ static esp_err_t pipeline_run(audio_run_fn run_fn, audio_event_iface_msg_t *msg)
 	return run_fn(msg);
 }
 
+void config_master(void) {
+	i2c_config_t conf_master = {
+		.mode             = I2C_MODE_MASTER,
+		.sda_io_num       = 18, // select GPIO specific to your project
+		.sda_pullup_en    = GPIO_PULLUP_ENABLE,
+		.scl_io_num       = 23, // select GPIO specific to your project
+		.scl_pullup_en    = GPIO_PULLUP_ENABLE,
+		.master.clk_speed = 10000, // select frequency specific to your project
+	};
+
+	ESP_ERROR_CHECK(i2c_param_config(I2C_NUM_0, &conf_master));
+	ESP_ERROR_CHECK(i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 1024, 0));
+}
+
+void send_command(uint8_t *message, size_t len) {
+	i2c_cmd_handle_t handle = i2c_cmd_link_create();
+	ESP_ERROR_CHECK(i2c_master_start(handle));
+	ESP_ERROR_CHECK(
+	    i2c_master_write_byte(handle, (0x69 << 1) | I2C_MASTER_WRITE, true));
+	ESP_ERROR_CHECK(i2c_master_write(handle, message, len, true));
+	ESP_ERROR_CHECK(i2c_master_stop(handle));
+	ESP_ERROR_CHECK(i2c_master_cmd_begin(I2C_NUM_0, handle, portMAX_DELAY));
+	i2c_cmd_link_delete(handle);
+}
+
+void turn_on_white_delay(void) {
+	for (int i = 0; i < 30; i++) {
+		uint8_t message[] = { LED_ON, i, 25, 25, 25 };
+		send_command(message, ARRAY_SIZE(message));
+		vTaskDelay(50 / portTICK_PERIOD_MS);
+	}
+}
+
+void turn_off(void) {
+	uint8_t message[] = { LED_OFF };
+	send_command(message, ARRAY_SIZE(message));
+}
+
 void app_main(void) {
 	app_init();
 
@@ -140,4 +188,16 @@ void app_main(void) {
 	}
 
 	app_free();
+
+	// CODE FOR SENDING COMMANDS TO LEDCONTROLLER
+
+	// config_master();
+
+	// while(true)
+	// {
+	//     turn_on_white_delay();
+	//     vTaskDelay(50/portTICK_PERIOD_MS);
+	//     turn_off();
+	//     vTaskDelay(1000/portTICK_PERIOD_MS);
+	// }
 }
