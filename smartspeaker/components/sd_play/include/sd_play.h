5f744d1f53faad1941ae461a13bbb53b3d7c87b0
Stef <s.kraaijeveld3@student.avans.nl>
2024-03-11 14:53:08 +0100

    Added functionality to play audio file .mp3

--- smartspeaker/components/sd_play/include/sd_play.h
+++ smartspeaker/components/sd_play/include/sd_play.h
@@ -3,10 +3,23 @@
 #pragma once
 
 /**
- * @brief TODO
- *
- * @return TODO
+ * @brief Sets .mp3 file up to be played.
+ * @param urlToAudioFile is a pointer to a string that is the path to the mp3
+ * file.
+ * @example playAudioThroughString("/sdcard/nl/cu.mp3");
  */
-void func(void);
+void playAudioThroughString(char *urlToAudioFile);
+
+/**
+ * @brief Sets .mp3 file up to be played.
+ * @param number is the number of the .mp3 file.
+ * @example playAudioThroughInt(13);
+ */
+void playAudioThroughInt(int number);
+
+/**
+ * @brief Inits pipeline for the sdcard and playing audio.
+ */
+void initSdcardClock(void);
 
 #endif /* SD_PLAY_H */
